286a7a79ef3f073e8f6c8c154ac13bb2f1a439b4
Mateo <mateoalzate07962@gmail.com>
2025-04-30 19:48:46 -0500

    new message

--- WebAppVeterinaria/Controllers/HomeController.cs
+++ WebAppVeterinaria/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace WebAppVeterinaria.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index1()
         {
             return View();
         }
